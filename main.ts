fd84df4e203bc083dfad0c079d0b6522c6e1e0a2
CaritoMai83 <83616190+CaritoMai83@users.noreply.github.com>
2021-05-03 22:56:00 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,124 @@
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # # . .
+    # . . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # # . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # # # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # . . . .
+    # . . . .
+    # # # # .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    # . . . .
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . #
+    . . . . #
+    . # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . . #
+    . . . . #
+    . # # # #
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . # #
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showIcon(IconNames.Sad)
+basic.pause(50)
+music.playMelody("C5 B A G F E D C ", 240)
